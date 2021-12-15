09851827c6c5baf0a261838859c91a93de710ab1
WicklowTiger <razvan.miculit@gmail.com>
2021-12-15 20:52:51 +0200

    chore: added Action Obj/Type

---
+++ src/main/java/shared/Action.java
@@ -0,0 +1,21 @@
+package shared;
+
+public class Action {
+    public ActionType actionType = null;
+    public String payload = null;
+
+    public Action(ActionType actionType, String payload) {
+        this.actionType = actionType;
+        this.payload = payload;
+    }
+
+    public Action() {
+
+    }
+
+    public static Integer generateKey() {
+        return key++;
+    }
+
+    private static Integer key = 0;
+}
---
+++ src/main/java/shared/ActionType.java
@@ -0,0 +1,8 @@
+package shared;
+
+public enum ActionType {
+    UPDATE_STOCKS,
+    ACK_REPLY,
+    SEND_BUY,
+    SEND_SELL
+}
