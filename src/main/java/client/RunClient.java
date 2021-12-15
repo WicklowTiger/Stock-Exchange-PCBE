b9fd21be17e059741dd52c7180dfdf147446fc11
WicklowTiger <razvan.miculit@gmail.com>
2021-12-15 21:01:01 +0200

    chore: updated run scripts

--- src/main/java/client/RunClient.java
+++ src/main/java/client/RunClient.java
@@ -1,10 +1,35 @@
 package client;
 
+import client.jfx.App;
+import client.jfx.HomeWindowController;
+import javafx.application.Application;
+import shared.Action;
+import shared.ActionType;
+import shared.Stock;
+
+import java.util.concurrent.TimeUnit;
+
 public class RunClient {
     public static void main(String[] args) {
-        TradeManager tradeManager = TradeManager.getInstance();
-        while (true) {
-            //...
+//
+        Thread jfxThread = new Thread(() -> Application.launch(App.class));
+        jfxThread.start();
+        App myApp = App.waitForApp();
+
+        ClientActionsManager clientActionsManager = ClientActionsManager.getInstance();
+        clientActionsManager.run();
+        try {
+            TimeUnit.MILLISECONDS.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        ClientActionsManager.putAction(new Action(ActionType.SEND_BUY, "AAPL,0.5,34.34"));
+        ClientActionsManager.putAction(new Action(ActionType.SEND_BUY, "AAPL,0.5"));
+        try {
+            TimeUnit.MILLISECONDS.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ClientActionsManager.putAction(new Action(ActionType.SEND_SELL, "AAPL,1.7"));
     }
 }
