cf4acd67d2262da27424812ababb914fc42a734f
WicklowTiger <razvan.miculit@gmail.com>
2021-12-16 19:33:36 +0200

    refactor: topic enum now has string conversion methods

--- src/main/java/shared/Topic.java
+++ src/main/java/shared/Topic.java
@@ -2,6 +2,43 @@ package shared;
 
 public enum Topic {
     STOCK_UPDATES,
+    TRADE_MESSAGES,
+    KEEP_ALIVE,
     TRADE_REPLIES,
-    NONE
+    USER_UPDATES,
+    NONE;
+
+    static String toString(Topic topic) {
+        switch (topic) {
+            case TRADE_REPLIES:
+                return "tradeReplies";
+            case STOCK_UPDATES:
+                return "stockUpdates";
+            case KEEP_ALIVE:
+                return "keepAlive";
+            case USER_UPDATES:
+                return "userUpdates";
+            case TRADE_MESSAGES:
+                return "tradeMessages";
+            default:
+                return "none";
+        }
+    }
+
+    static Topic fromString(String str) {
+        switch (str) {
+            case "tradeReplies":
+                return Topic.TRADE_REPLIES;
+            case "stockUpdates":
+                return Topic.STOCK_UPDATES;
+            case "keepAlive":
+                return Topic.KEEP_ALIVE;
+            case "userUpdates":
+                return Topic.USER_UPDATES;
+            case "tradeMessages":
+                return Topic.TRADE_MESSAGES;
+            default:
+                return Topic.NONE;
+        }
+    }
 }
