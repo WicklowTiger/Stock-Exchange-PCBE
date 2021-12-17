83e103d18e75da908c7dc3f39933582bc3f073ae
WicklowTiger <razvan.miculit@gmail.com>
2021-12-17 23:00:24 +0200

    chore: added secondary run script to prove multi-client functionality

--- src/main/java/client/RunClient1.java
+++ src/main/java/client/RunClient1.java
@@ -4,13 +4,13 @@ import client.jfx.App;
 import javafx.application.Application;
 import shared.Const;
 
-public class RunClient {
+public class RunClient1 {
     public static void main(String[] args) {
         Thread jfxThread = new Thread(() -> Application.launch(App.class));
         jfxThread.start();
         App myApp = App.waitForApp();
 
-        ClientActionsManager clientActionsManager = ClientActionsManager.getInstance(Const.defaultUser.uid);
+        ClientActionsManager clientActionsManager = ClientActionsManager.getInstance(Const.defaultUser2.uid);
         clientActionsManager.run();
     }
 }
